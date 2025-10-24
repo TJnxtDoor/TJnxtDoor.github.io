f9ffeb77f55b5167ace28f8cf2a72be2ee567300
TJ <afzaltj81@gmail.com>
2025-10-24 10:09:41 -0400

    added JS to multple index files

--- API.js
+++ API.js
@@ -1,16 +1,13 @@
-async function getData() {
-  const url = ("https://api.github.com/users/tjnxtdoor/repos")
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
+fetch("https://api.github.com/users/tjnxtdoor/repos")
+  .then(res => res.json())
+  .then(repos => {
+    const list = document.createElement("ul");
+    repos.forEach(repo => {
+      const li = document.createElement("li");
+      li.textContent = repo.name;
+      list.appendChild(li);
+    });
+    document.body.appendChild(list);
+  })
+  .catch(err => console.error(err));
 
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-getData();
